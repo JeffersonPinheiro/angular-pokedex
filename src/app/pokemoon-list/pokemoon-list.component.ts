416f41ab7e2d0efd313c10fe7fe2169abeb6ad78
Jefferson Pinheiro <jeffersonpinheiro194@gmail.com>
2023-02-08 01:12:23 -0300

    Adição de componentes
    Adicionados componentes: Cards/Card-List

---
+++ src/app/pokemoon-list/pokemoon-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemoon-list',
+  templateUrl: './pokemoon-list.component.html',
+  styleUrls: ['./pokemoon-list.component.sass']
+})
+export class PokemoonListComponent {
+
+}
