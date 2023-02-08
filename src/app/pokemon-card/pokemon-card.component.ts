c7627bc53baa2d19a10035850e285581fe54b78d
Jefferson Pinheiro <jeffersonpinheiro194@gmail.com>
2023-02-08 20:35:08 -0300

    Adicionando pokemon-cards
    feat: pokemon-card adicionados

--- src/app/pokemon-card/pokemon-card.component.ts
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Pokemon } from 'C:/Users/Jefferson Pinheiro/source/repos/angular-sandbox/src/_model/Pokemon';
 
 @Component({
   selector: 'app-pokemon-card',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./pokemon-card.component.sass']
 })
 export class PokemonCardComponent {
-
+  @Input()
+  public pokemon!: Pokemon;
 }
--- src/app/pokemoon-list/pokemoon-list.component.ts
+++ src/app/pokemoon-list/pokemoon-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Type } from 'C:/Users/Jefferson Pinheiro/source/repos/angular-sandbox/src/_model/Type';
+import { Pokemon } from 'C:/Users/Jefferson Pinheiro/source/repos/angular-sandbox/src/_model/Pokemon';
 
 @Component({
   selector: 'app-pokemoon-list',
@@ -6,5 +8,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./pokemoon-list.component.sass']
 })
 export class PokemoonListComponent {
-
+  public pokemons: Pokemon[] = [
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png',
+      number: 1,
+      name: 'Bulbasaur',
+      types: [
+        Type.Grass,
+        Type.Poison
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/002.png',
+      number: 2,
+      name: 'Ivysaur',
+      types: [
+        Type.Grass,
+        Type.Poison
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/003.png',
+      number: 3,
+      name: 'Venosaur',
+      types: [
+        Type.Grass,
+        Type.Poison
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/004.png',
+      number: 4,
+      name: 'Charmander',
+      types: [
+        Type.Fire,
+      ],
+    },
+  ];
 }
+
+
+
